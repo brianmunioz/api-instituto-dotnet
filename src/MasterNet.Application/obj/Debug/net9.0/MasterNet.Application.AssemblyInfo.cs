886142e8dd33cb2a205dadd89bd2e87af592aea4
Brian Muñoz <brianmunozjob@gmail.com>
2024-12-04 22:26:30 -0300

    AGREGAR: Interfaz para reportes

--- src/MasterNet.Application/obj/Debug/net9.0/MasterNet.Application.AssemblyInfo.cs
+++ src/MasterNet.Application/obj/Debug/net9.0/MasterNet.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasterNet.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b6062737f76a176cc855c493d0b623d27b9acee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131a9e39b32aa038858a8dad4c97431f53d14c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasterNet.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MasterNet.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
